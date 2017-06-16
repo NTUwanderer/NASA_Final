b8c313d24c4791637e3e2026c790397ae326694e
NTUwanderer <harveyycyang@gmail.com>
2017-06-16 04:29:10 -0400

    Add nfs/setup.sh & nfs-utils in server/setup.sh

---
+++ nfs/setup.sh
@@ -0,0 +1,20 @@
+dhclient -v
+yum install -y vim git gcc-c++ make systemd-networkd autofs
+git clone https://github.com/NTUwanderer/NASA_Final
+
+yum install -y nfs-utils
+mkdir /var/nfsshare
+chmod -R 777 /var/nfsshare/
+echo "/var/nfsshare 192.168.100.1(rw,sync,no_root_squash,no_all_squash)" >> /etc/exports
+
+systemctl enable rpcbind
+systemctl enable nfs-server
+systemctl enable nfs-lock
+systemctl enable nfs-idmap
+systemctl start rpcbind
+systemctl start nfs-server
+systemctl start nfs-lock
+systemctl start nfs-idmap
+firewall-cmd --permanent --zone=public --add-service=nfs
+firewall-cmd --reload
+
--- server/setup.sh
+++ server/setup.sh
@@ -1,4 +1,4 @@
 dhclient -v
-yum install -y vim git gcc-c++ make systemd-networkd
+yum install -y vim git gcc-c++ make systemd-networkd autofs nfs-utils
 git clone https://github.com/NTUwanderer/NASA_Final
 
