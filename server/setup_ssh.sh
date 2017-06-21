0e2572de7b1523e4e75e4d80237719fc837cd300
NTUwanderer <harveyycyang@gmail.com>
2017-06-21 18:20:02 -0400

    Add server/setup_ssh.sh

---
+++ server/setup_ssh.sh
@@ -0,0 +1,13 @@
+# input number ip
+
+ssh-keygen -b 4096 -t rsa -C "nfs$1" -N nasa-nfs"$1" -f ~/.ssh/id_rsa"$1"
+scp ~/.ssh/id_rsa"$1".pub root@"$2":~/.ssh/
+ssh root@"$2" << EOF
+	cat ~/.ssh/id_rsa"$1".pub >> ~/.ssh/authorized_keys
+	echo -e "\n" >> ~/.ssh/authorized_keys
+	rm ~/.ssh/id_rsa"$1".pub
+	restorecon -Rv ~/.ssh
+	systemctl restart sshd
+EOF
+
+echo -e "Host nasa-nfs$1\n\tHostName $2\n\tUser root\n\tIdentityFile ~/.ssh/id_rsa$1\n\n" >> ~/.ssh/config
